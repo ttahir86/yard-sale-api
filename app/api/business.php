e0eeed1af03423f0494e7cca5d4beb45c8b5fce5
talal tahir <ttahir@billtrust.com>
2018-05-10 17:09:38 -0400

    Some refactoring of the create new business endpoint

--- app/api/business.php
+++ app/api/business.php
@@ -1,62 +1,60 @@
 <?php 
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
+require_once('businessClass.php');
+
 
 $app->get('/api/business', function() {
-	
-	// require_once(__DIR__.'/../../database/SQLQuery.php');
-	echo "business";
-	require_once('businessClass.php');
-
-	$business = new Business;
-
+	$business = new Business();
+	// THIS SHOULD BE A PUBLIC STATIC METHOD.
 	$allBusinesses = $business->getAllBusiness();
 	echo $allBusinesses;
-
-
-	// $query = "select * from businessname";
-	// $result = $conn->prepare($query);
-	// $result->execute();
-
-	// foreach ($result as $row) {
-	// 	$data[] = $row;
-	// 	// print_r($row);
-	// }
-
-	// if (isset($data)) {
-	// 	header('Content-Type: application/json');
-	// 	echo json_encode($data);
-	// }
-
-
 });
 
 
 
-$app->post('/api/addbusiness', function() {
 
-	require_once('businessClass.php');
+/*
+	@type:		POST
+	@params: 	business_name 		
+	@params:	email_address
+	@params: 	password
+	@return: 	message indicating success or failure upon enrollment
+*/
+$app->post('/api/addbusiness', function(Request $request, Response $response) {
 
+	
+	
+	// get params from request
+	$business_name 	= $request->getParam('business_name') 	== null || 	$request->getParam('business_name') 	== "" 	: null ? $request->getParam('business_name');
+	$email_address 	= $request->getParam('email_address') 	== null || 	$request->getParam('email_address') 	== "" 	: null ? $request->getParam('email_address');
+	$password 		= $request->getParam('password') 		== null || 	$request->getParam('password') 			== "" 	: null ? $request->getParam('password');
+	
+	// create array with business information 
+	$businessInfo = array(
+		"business_name" => $business_name,
+		"email_address" => $email_address,
+		"password"		=> $password
+	);
+	
+	// create new business
+	$newBusiness = _addNewBusiness($businessInfo);
+	
+	// return response
+	$message = $newBusiness: "BUSINESS CREATED SUCCESFFULLY <BR>" ? "FAILURE - BUSINESS COULD NOT BE CREATED.";
+	
+	echo message;
+
+
+});
+
+protected function _addNewBusiness($businessInfo){
 	$newBusiness = new Business();
-	$aBusinessInfo = $_POST['aInfo'];
-
-	$business_name = $aBusinessInfo['business_name'];
-	$email_address = $aBusinessInfo['email_address'];
-	$password	   = $aBusinessInfo['password'];
-
-	$newBusiness->setBusinessName($business_name);
-	$newBusiness->setEmailAddress($email_address);
-	$newBusiness->setPassword($password);
-
-	echo "***BUSIENESS PHP WITH POST DATA"."<BR>";
-	var_dump($newBusiness);
-	echo "<BR>";
-
-
+	$newBusiness->setBusinessName($businessInfo['business_name']);
+	$newBusiness->setEmailAddress($businessInfo['email_address']);
+	$newBusiness->setPassword($businessInfo['password']);
 	$newBusiness->createBusiness();
-
-
-});
-
+	return $newBusiness;
+}
 
 ?>
